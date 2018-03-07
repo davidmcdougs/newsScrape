c0f7148c2d7e3a1c7261ee45e549a9c7d29d59c4
davidmcdougs <davidl.mcdougald@gmail.com>
2018-03-06 20:48:15 -0500

    made it look better

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><a href ='"+data[i].link+"'>Original Article </a><br /> <a data-id='" + data[i]._id + "' href='' class = 'comments'>Comments</a></p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><a href ='"+data[i].link+"'>Original Article </a><br /> <a data-id='" + data[i]._id + "' href='' class = 'comments grow'>Comments</a></p>");
   }
 });
 
